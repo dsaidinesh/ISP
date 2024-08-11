101a5d13415cdaf7b84e4d33bf65b4cd2ecc1b2e
dsaidinesh <dsaidinesh2003@gmail.com>
2024-08-11 21:05:46 +0530

    some changes in styling

--- static/script.js
+++ static/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     if(window.location.hash) {
-        var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
+        var hash = window.location.hash.substring(1);
         var hashTab = document.getElementById(hash + '-tab');
         if (hashTab) {
-            new bootstrap.Tab(hashTab).show(); // Show the tab corresponding to the hash
+            new bootstrap.Tab(hashTab).show();
         }
     }
 });
